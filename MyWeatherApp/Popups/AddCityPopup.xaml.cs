5bfa14e865c666587fe07cece6c0f55bb31ee362
Sebastian Ortiz Velez <sebastia.ortiz@starshotsoftware.com>
2018-07-01 01:50:27 +0200

    Create structure for add city popup page

---
+++ MyWeatherApp/Popups/AddCityPopup.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Rg.Plugins.Popup.Extensions;
+using Xamarin.Forms;
+
+namespace MyWeatherApp.Popups
+{
+	public partial class AddCityPopup : Rg.Plugins.Popup.Pages.PopupPage
+    {
+		public AddCityPopup()
+        {
+            InitializeComponent();
+        }
+
+        async void GoBack()
+        {
+            await Navigation.PopPopupAsync();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+
+        // ### Overrided methods which can prevent closing a popup page ###
+
+        // Invoked when a hardware back button is pressed
+        protected override bool OnBackButtonPressed()
+        {
+            // Return true if you don't want to close this popup page when a back button is pressed
+            return base.OnBackButtonPressed();
+        }
+
+        // Invoked when background is clicked
+        protected override bool OnBackgroundClicked()
+        {
+            // Return false if you don't want to close this popup page when a background of the popup page is clicked
+            return base.OnBackgroundClicked();
+        }
+    }
+}
