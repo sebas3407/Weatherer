16236a1caef726184e2427e288dbf0e5d82c9d55
Sebastian Ortiz Velez <sebastia.ortiz@starshotsoftware.com>
2018-07-01 16:23:47 +0200

    Added "Addcity viewmodel"

---
+++ MyWeatherApp/ViewModels/AddCityViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MyWeatherApp.ViewModels
+{
+	public class AddCityViewModel : BaseViewModel
+    {
+        public AddCityViewModel()
+        {
+        }
+
+		//call example http://api.openweathermap.org/data/2.5/forecast/daily?q=cali&cnt=5&units=metric&appid=50d4d8b59f8c1a0a41360976992f86f1
+    }
+}
