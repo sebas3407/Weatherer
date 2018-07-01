b0495771df99d1382ff24150168ef5011fa1dfa4
Sebastian Ortiz Velez <sebastia.ortiz@starshotsoftware.com>
2018-07-01 16:08:53 +0200

    Create the template for the popup

--- MyWeatherApp/ViewModels/SettingsViewModel.cs
+++ MyWeatherApp/ViewModels/SettingsViewModel.cs
@@ -1,18 +1,23 @@
 ﻿using System;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Xamarin.Forms;
+using Rg.Plugins.Popup.Extensions;
+using MyWeatherApp.Popups;
+
 namespace MyWeatherApp.ViewModels
 {
 	public class SettingsViewModel : BaseViewModel
     {
-		private string test;
-        public string Test
+		public RelayCommand NewCityPopupCommand { get; set; }
+		async void NewCityPopup()
         {
-            get { return test; }
-			set { SetValue(ref test, value); } 
-
+			await Application.Current.MainPage.Navigation.PushPopupAsync(new AddCityPopup());
         }
+
         public SettingsViewModel()
         {
-			Test = "Testing";
+    	    NewCityPopupCommand = new RelayCommand(NewCityPopup);
         }
     }
 }
