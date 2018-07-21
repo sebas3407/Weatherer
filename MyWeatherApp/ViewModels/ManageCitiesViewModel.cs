b70aa145f123fa7846b90c33a32456437d7533b9
Sebastian Ortiz Velez <sebastia.ortiz@starshotsoftware.com>
2018-07-21 02:00:01 +0200

    Working on current geolocation

--- MyWeatherApp/ViewModels/ManageCitiesViewModel.cs
+++ MyWeatherApp/ViewModels/ManageCitiesViewModel.cs
@@ -1,22 +1,43 @@
-﻿using GalaSoft.MvvmLight.Command;
+﻿using System;
+using GalaSoft.MvvmLight.Command;
 using MyWeatherApp.Popups;
+using Plugin.Geolocator;
 using Rg.Plugins.Popup.Extensions;
 using Xamarin.Forms;
 
 namespace MyWeatherApp.ViewModels
 {
-	public class ManageCitiesViewModel
-    {
-		public RelayCommand NewCityPopupCommand { get; set; }
-        async void NewCityPopup()
-        {
-            MainViewModel.GetInstance().AddCity = new AddCityViewModel();
-            await Application.Current.MainPage.Navigation.PushPopupAsync(new AddCityPopup());
-        }
+	public class ManageCitiesViewModel : BaseViewModel
+	{
+		#region Properties
+		double latitude;
+		double longitude;
+		#endregion
 
+		#region Methods
+		public RelayCommand NewCityPopupCommand { get; set; }
+		async void NewCityPopup()
+		{
+			MainViewModel.GetInstance().AddCity = new AddCityViewModel();
+			GetCurrentLocation();
+			await Application.Current.MainPage.Navigation.PushPopupAsync(new AddCityPopup());
+		}
+
+		public async void GetCurrentLocation()
+		{
+			var locator = CrossGeolocator.Current;
+			var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+
+			latitude = position.Latitude;
+			longitude = position.Longitude;
+		}
+		#endregion
+
+		#region Constructor
 		public ManageCitiesViewModel()
-        {
-            NewCityPopupCommand = new RelayCommand(NewCityPopup);
-        }
-    }
+		{
+			NewCityPopupCommand = new RelayCommand(NewCityPopup);
+		}
+		#endregion
+	}
 }
