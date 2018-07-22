34712f42643b1a4b24b541fa537b3f1f1878c4ee
Sebastian Ortiz Velez <sebastia.ortiz@starshotsoftware.com>
2018-07-22 22:04:19 +0200

    Added Internet connection service

---
+++ MyWeatherApp/Responses/Response.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MyWeatherApp.ViewModels;
+
+namespace MyWeatherApp.Responses
+{
+    public class Response : BaseViewModel
+    {
+        private bool isSuccess;
+        public bool IsSuccess
+        {
+            get { return isSuccess; }
+            set { SetValue(ref isSuccess, value); }
+        }
+
+		private String message;
+		public String Message
+        {
+			get { return message; }
+			set { SetValue(ref message, value); }
+        }
+    }
+}
---
+++ MyWeatherApp/Services/InternetConnection.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using MyWeatherApp.Responses;
+using Plugin.Connectivity;
+
+namespace MyWeatherApp.Services
+{
+    public class InternetConnection
+    {
+		public async Task<Response> CheckConnection()
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "Please turn on your internet settings.",
+                };
+            }
+
+            var isReachable = await CrossConnectivity.Current.IsRemoteReachable(
+                "google.com");
+			
+            if (!isReachable)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "Check you internet connection.",
+                };
+            }
+
+            return new Response
+            {
+                IsSuccess = true,
+                Message = "Ok",
+            };
+        }
+    }
+}
