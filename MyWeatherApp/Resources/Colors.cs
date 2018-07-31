fe4bc66b2253e09240c72758a115dc3371959644
Sebastian Ortiz Velez <sebastia.ortiz@starshotsoftware.com>
2018-07-31 15:01:51 +0200

    update some design issues

--- MyWeatherApp/Resources/Colors.cs
+++ MyWeatherApp/Resources/Colors.cs
@@ -4,8 +4,13 @@ namespace MyWeatherApp.Resources
 {
     public static class Colors
     {
+		//public static readonly Color MainColor = Color.FromRgb(255, 105, 26);
+		//public static readonly Color MainColor = Color.FromRgb(178, 93, 184);
+		//public static readonly Color MainTextColor = Color.Black;
+		//public static readonly Color TextColor = Color.White;
+
 		public static readonly Color MainColor = Color.FromHex("ff6e66");
-		public static readonly Color MainTextColor = Color.FromHex("EC4C47");
-		public static readonly Color TextColor = Color.White;
+        public static readonly Color MainTextColor = Color.FromHex("EC4C47");
+        public static readonly Color TextColor = Color.White;
     }
 }
