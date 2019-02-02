b3edefd0ad66dda67ebc19ad965209373076a65a
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2019-02-02 23:36:58 +0100

    Change default background and disabled temporally the function to add more cities

--- MyWeatherApp/Pages/HomePage.xaml.cs
+++ MyWeatherApp/Pages/HomePage.xaml.cs
@@ -10,16 +10,16 @@ namespace MyWeatherApp.Pages
         public HomePage()
         {
             InitializeComponent();
+            /*
 			ToolbarItems.Add(new ToolbarItem("Cities", "cities.png", async () =>
             {
 				MainViewModel.GetInstance().ManageCity = new ManageCitiesViewModel();
 				await Navigation.PushAsync(new ManageCitiesPage(), true);
-            }));
+            }));*/
             if (MainViewModel.GetInstance().Home.Clouds == "") 
             {
 
             }
-            this.BackgroundImage = "background.png";
         }
 
 		protected override void OnAppearing()
